Fix ddp loss calculation to use correct data

dawn/scripts/segfault_debug/my_ddp.py
@@ -10,6 +10,8 @@ import torch
 import torch.nn as nn
 import xarray as xr
 from aurora_loss import mae
+from load_batches import get_gt_batch, get_input_batch
+from load_data import load_data
 from torch.distributed import init_process_group
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -21,6 +23,7 @@ os.environ["RANK"] = RANK
 
 # PMI_SIZE set by mpirun
 WORLD_SIZE = os.environ["PMI_SIZE"]
+assert WORLD_SIZE == "2"
 os.environ["WORLD_SIZE"] = WORLD_SIZE
 
 os.environ["MASTER_ADDR"] = "0.0.0.0"
@@ -34,6 +37,9 @@ def main():
         backend="ccl",
     )
 
+    device = f"xpu:{RANK}"
+    print(f"Using {device=}")
+
     print("loading model...")
     model = Aurora(
         use_lora=False,  # Model was not fine-tuned.
@@ -44,61 +50,26 @@ def main():
     download_path = Path("../../era5/era_v_inf")
 
     print("loading data...")
-    static_vars_ds = xr.open_dataset(download_path / "static.nc", engine="netcdf4")
-    surf_vars_ds = xr.open_dataset(
-        download_path / "2023-01-01-surface-level.nc", engine="netcdf4"
-    )
-    atmos_vars_ds = xr.open_dataset(
-        download_path / "2023-01-01-atmospheric.nc", engine="netcdf4"
-    )
-
-    # i = 1  # Select this time index in the downloaded data.
 
     # 1 for RANK 0 and 3 for RANK 1.
     i = (int(RANK) * 2) + 1
-
     print(f"batching with {i=}")
-    batch = Batch(
-        surf_vars={
-            # First select time points `i` and `i - 1`. Afterwards, `[None]` inserts a
-            # batch dimension of size one.
-            "2t": torch.from_numpy(surf_vars_ds["t2m"].values[[i - 1, i]][None]),
-            "10u": torch.from_numpy(surf_vars_ds["u10"].values[[i - 1, i]][None]),
-            "10v": torch.from_numpy(surf_vars_ds["v10"].values[[i - 1, i]][None]),
-            "msl": torch.from_numpy(surf_vars_ds["msl"].values[[i - 1, i]][None]),
-        },
-        static_vars={
-            # The static variables are constant, so we just get them for the first time.
-            "z": torch.from_numpy(static_vars_ds["z"].values[0]),
-            "slt": torch.from_numpy(static_vars_ds["slt"].values[0]),
-            "lsm": torch.from_numpy(static_vars_ds["lsm"].values[0]),
-        },
-        atmos_vars={
-            "t": torch.from_numpy(atmos_vars_ds["t"].values[[i - 1, i]][None]),
-            "u": torch.from_numpy(atmos_vars_ds["u"].values[[i - 1, i]][None]),
-            "v": torch.from_numpy(atmos_vars_ds["v"].values[[i - 1, i]][None]),
-            "q": torch.from_numpy(atmos_vars_ds["q"].values[[i - 1, i]][None]),
-            "z": torch.from_numpy(atmos_vars_ds["z"].values[[i - 1, i]][None]),
-        },
-        metadata=Metadata(
-            lat=torch.from_numpy(surf_vars_ds.latitude.values),
-            lon=torch.from_numpy(surf_vars_ds.longitude.values),
-            # Converting to `datetime64[s]` ensures that the output of `tolist()` gives
-            # `datetime.datetime`s. Note that this needs to be a tuple of length one:
-            # one value for every batch element.
-            time=(surf_vars_ds.valid_time.values.astype("datetime64[s]").tolist()[i],),
-            atmos_levels=tuple(
-                int(level) for level in atmos_vars_ds.pressure_level.values
-            ),
-        ),
+
+    # Load data
+    static_vars_ds, surf_vars_ds, atmos_vars_ds = load_data(download_path)
+
+    # Get input
+    batch = get_input_batch(i, static_vars_ds, surf_vars_ds, atmos_vars_ds).to(device)
+
+    # Get output
+    ground_truth = get_gt_batch(i, static_vars_ds, surf_vars_ds, atmos_vars_ds).to(
+        device
     )
 
     print("preparing model...")
     model.configure_activation_checkpointing()
-    model = DDP(model).to(f"xpu:{RANK}")
+    model = DDP(model).to(device)
     model.train()
-
-    batch = batch.to(f"xpu:{RANK}")
 
     # AdamW, as used in the paper.
     optimizer = torch.optim.AdamW(model.parameters())
@@ -115,8 +86,7 @@ def main():
 
         # mean absolute error of one variable
         print("calculating loss...")
-        # loss = torch.mean(torch.abs(pred.surf_vars["2t"] - batch.surf_vars["2t"][:,:,:720,:]))
-        loss = mae(pred, batch)
+        loss = mae(pred, ground_truth)
 
         print("performing backward pass...")
         loss.backward()