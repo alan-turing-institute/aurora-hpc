061b212f0ed3fb859b3d464b8b42d03f58e868e1
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2025-07-02 19:27:44 +0100

    Support multi-node training on Baskeville
    Applies a number of small changes to get multi-node training on Baskerville.
    
    Moves the training times to the GPU to avoid the runtime error: "Tensors must
    be CUDA and dense"
    
    Also sets up the sbatch script to use srun to distributed process across nodes.

--- train/batch/bask-train-fsdp.sh
+++ train/batch/bask-train-fsdp.sh
@@ -2,11 +2,12 @@
 # vim: et:ts=4:sts=4:sw=4
 #SBATCH --qos turing
 #SBATCH --account usjs9456-ati-test
-#SBATCH --time 0:10:0
-#SBATCH --nodes 1
+#SBATCH --time 0:20:0
+#SBATCH --nodes 2
+#SBATCH --ntasks-per-node 1
 #SBATCH --gpus-per-node 2
 #SBATCH --cpus-per-gpu 36
-#SBATCH --mem 32768
+#SBATCH --mem 65536
 #SBATCH --constraint=a100_80
 #SBATCH --job-name aurora-train
 #SBATCH --output log-train.txt
@@ -22,7 +23,7 @@ set -e
 
 pushd ../scripts
 
-if [ ! -d ../downloads ]; then
+if [ ! -d ../../downloads ]; then
   echo "Please run the batch-download.sh script to download the data."
   exit 1
 fi
@@ -39,6 +40,8 @@ module -q load torchvision/0.15.2-foss-2022a-CUDA-11.7.0
 echo
 echo "## Configuring environment"
 
+export PRIMARY_PORT=$((16384 + $RANDOM % 16384))
+export PRIMARY_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export OMP_NUM_THREADS=1
 
 echo
@@ -52,6 +55,14 @@ pip install --quiet xarray==2023.1.0
 pip install --quiet -e ../../aurora
 
 echo
+echo "## Details"
+echo
+echo "Nodes: ${SLURM_GPUS_PER_NODE}"
+echo "GPUs per node: ${SLURM_GPUS_PER_NODE}"
+echo "Primary address: ${PRIMARY_ADDR}"
+echo "Primary port: ${PRIMARY_PORT}"
+
+echo
 echo "## Running model"
 
 # Track GPU and CPU metrics
@@ -59,7 +70,15 @@ nvidia-smi dmon -o TD -s puct -d 1 > log-train-gpu.txt &
 vmstat -t 1 -y > log-train-cpu.txt &
 
 # Perform the prediction
-python -m torch.distributed.run --nnodes 1 --nproc-per-node 2 train.py
+srun bash -c \
+    'python -m torch.distributed.run \
+    --nnodes ${SLURM_JOB_NUM_NODES} \
+    --nproc-per-node ${SLURM_GPUS_PER_NODE} \
+    --master_addr ${PRIMARY_ADDR} \
+    --master_port ${PRIMARY_PORT} \
+    --node_rank ${SLURM_NODEID} \
+    train.py \
+    -d ../../downloads'
 
 echo
 echo "## Tidying up"
