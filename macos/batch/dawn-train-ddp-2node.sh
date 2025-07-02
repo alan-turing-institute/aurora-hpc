3beddc2af54ca45e7d1e985e6e52a7d17dbec261
Rosie Wood <rwood@turing.ac.uk>
2025-07-02 14:04:13 +0100

    add/update dawn batch scripts

--- macos/batch/dawn-train-ddp-2node.sh
+++ macos/batch/dawn-train-ddp-2node.sh
@@ -2,9 +2,10 @@
 #SBATCH --job-name=example-ddp
 #SBATCH --account=airr-p8-rcpp-dawn-gpu
 #SBATCH --partition=pvc9 # Dawn PVC partition
-#SBATCH -n 4   # Number of tasks (usually number of MPI ranks)
+#SBATCH -n 8   # Number of tasks (usually number of MPI ranks)
 #SBATCH -c 24  # Number of cores per task
 #SBATCH --gres=gpu:4 # Number of requested GPUs per node
+#SBATCH -N 2 # 2 nodes
 #SBATCH --time 01:00:00
 
 #set -o xtrace
@@ -37,7 +38,7 @@ export ZES_ENABLE_SYSMAN=1
 # Otherwise we're told to.
 export CCL_ZE_IPC_EXCHANGE=sockets
 
-mpirun -prepend-rank -n 4 python my_ddp.py --xpu -d ../../dawn/era5/era_v_inf/
+mpirun -prepend-rank -n 8 python my_ddp.py --xpu -d ../../dawn/era5/era_v_inf/
 
 deactivate
 popd
