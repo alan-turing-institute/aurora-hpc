3d0c7eff933a683cb7cdf1808e9ddfb7f0239734
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-07-29 11:33:35 +0100

    dict -> list[dict]

--- macos/scripts/test_utils.py
+++ macos/scripts/test_utils.py
@@ -1,11 +1,14 @@
+import os
 import pathlib
 import unittest
+from unittest import skip
 from unittest.mock import patch
 
 import utils
 
 
 class TestCudaMetrics(unittest.TestCase):
+
     def test_metrics_are_available(self):
         """CUDA metrics availability depends on the system."""
         with patch("torch.cuda.is_available", autospec=True, return_value=True):
@@ -15,11 +18,11 @@ class TestCudaMetrics(unittest.TestCase):
         """CUDA metrics should return a dictionary with expected keys."""
         cuda_metrics = utils.CudaMetrics()
         with patch("torch.cuda.utilization", autospec=True, return_value=75):
-            metrics = cuda_metrics.get_metrics()
+            metrics = cuda_metrics.get_metrics()[0]
         self.assertEqual(75, metrics["gpu_util"])
 
 
-class TestXpuMetrics(unittest.TestCase):
+class TestXpuMetricsV1(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -31,7 +34,9 @@ class TestXpuMetrics(unittest.TestCase):
 
     def setUp(self):
         """Set up the test environment."""
-        self.test_file.write_text("x\n1", encoding="utf-8")
+        # Note that xpu-smi files don't end in a newline.
+        text = "gpu,tile,util\n0,0,5\n0,1,3\n0,0,50\n0,1,30"
+        self.test_file.write_text(text, encoding="utf-8")
 
     def tearDown(self):
         """Clean up the test environment."""
@@ -43,13 +48,99 @@ class TestXpuMetrics(unittest.TestCase):
             "utils.which",
             autospec=True,
         ):
-            self.assertTrue(utils.XpuMetrics.are_available())
+            self.assertTrue(utils.XpuMetricsV1.are_available())
 
     def test_get_metrics(self):
 
         # 'dump', '--metrics', '0,1', '--device', '-1', '-n', '1'
-        metrics = utils.XpuMetrics(metrics_file=self.test_file).get_metrics()
-        self.assertEqual({"x": "1"}, metrics)
+        metrics = utils.XpuMetricsV1(metrics_file=self.test_file).get_metrics()
+        self.assertEqual({"gpu": "0", "tile": "0", "util": "50"}, metrics[0])
+        self.assertEqual({"gpu": "0", "tile": "1", "util": "30"}, metrics[1])
+
+    def test_get_metrics_invalid_device(self):
+
+        with self.assertRaises(utils.DeviceIdError) as context:
+            utils.XpuMetricsV1(metrics_file=self.test_file).get_last_device_entry(99)
+        self.assertEqual(
+            "Device ID 99 not found in the metrics file.", str(context.exception)
+        )
+
+    def test_previous_line(self):
+        xpu_metrics = utils.XpuMetricsV1(metrics_file=self.test_file)
+        with self.test_file.open("rb") as f:
+            f.seek(-1, os.SEEK_END)
+            line = xpu_metrics.get_previous_line(f)
+            self.assertEqual("0,1,30", line)
+            line = xpu_metrics.get_previous_line(f)
+            self.assertEqual("0,0,50", line)
+            line = xpu_metrics.get_previous_line(f)
+            self.assertEqual("0,1,3", line)
+            line = xpu_metrics.get_previous_line(f)
+            self.assertEqual("0,0,5", line)
+
+
+class TestXpuMetricsV2(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.old_path = os.environ["PATH"].split(":")
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up the test environment."""
+        cls.old_path = os.environ["PATH"].split(":")
+        os.environ["PATH"] = ":".join(["xpu_smi_dir/"] + cls.old_path)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up the test environment."""
+        os.environ["PATH"] = ":".join(cls.old_path)
+
+    def test_metrics_are_available(self):
+        """XPU metrics availability depends on the system."""
+        with patch(
+            "utils.which",
+            autospec=True,
+        ):
+            self.assertTrue(utils.XpuMetricsV2.are_available())
+
+    def test_get_metrics(self):
+        metrics = utils.XpuMetricsV2().get_metrics()
+        reduced_metrics = [
+            {
+                k: v
+                for k, v in metric.items()
+                if k in ["Timestamp", "DeviceId", "TileId", "Compute Engine 0 (%)"]
+            }
+            for metric in metrics
+        ]
+
+        self.assertListEqual(
+            [
+                {
+                    "Timestamp": "16:53:07.186",
+                    "DeviceId": "0",
+                    "TileId": "0",
+                    "Compute Engine 0 (%)": "0.00",
+                },
+                {
+                    "Timestamp": "16:53:07.187",
+                    "DeviceId": "0",
+                    "TileId": "1",
+                    "Compute Engine 0 (%)": "0.00",
+                },
+            ],
+            reduced_metrics,
+        )
+
+    def test_get_metrics_two(self):
+        with patch("utils.run", autospec=True) as mock_run:
+            utils.XpuMetricsV2().get_metrics()
+            mock_run.assert_called_once_with(
+                ["xpu-smi", "dump", "--metrics", "0,1", "--device", "-1", "-n", "1"],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
 
 
 if __name__ == "__main__":
--- macos/scripts/utils.py
+++ macos/scripts/utils.py
@@ -2,6 +2,7 @@ import os
 from abc import ABC, abstractmethod
 from pathlib import Path
 from shutil import which
+from subprocess import run
 from typing import Any
 
 import torch
@@ -9,7 +10,7 @@ import torch
 
 class UsageMetrics(ABC):
     @abstractmethod
-    def get_metrics(self) -> dict[str, Any]:
+    def get_metrics(self) -> list[dict[str, Any]]:
         """Get device metrics."""
 
     @classmethod
@@ -18,7 +19,15 @@ class UsageMetrics(ABC):
         """Whether metrics are available."""
 
 
-class XpuMetrics(UsageMetrics):
+class DeviceIdError(Exception):
+    """Exception raised when a device ID is not found in the metrics file."""
+
+    def __init__(self, device_id: int):
+        super().__init__(f"Device ID {device_id} not found in the metrics file.")
+        self.device_id = device_id
+
+
+class XpuMetricsV1(UsageMetrics):
     """Note that we expect metrics to be streaming to a file already."""
 
     def __init__(self, *args, metrics_file: Path = Path("xpu_metrics.txt"), **kwargs):
@@ -27,31 +36,85 @@ class XpuMetrics(UsageMetrics):
         self.metrics_file = metrics_file
         if self.metrics_file.exists():
             with self.metrics_file.open("r", encoding="utf-8") as f:
-                self.first_line = f.readline().strip()
-        # Todo: parse from CSV into columns
+                first_line = f.readline().strip()
+        self.headers = first_line.split(",")
 
     @classmethod
     def are_available(cls) -> bool:
         """Check whether XPU is available."""
         return which("xpu-smi") is not None
 
-    def get_last_line(self) -> str:
+    @staticmethod
+    def get_previous_line(f) -> str:
+        """Get the previous line of the metrics file."""
+        # Read backwards until we find a newline
+        while f.read(1) != b"\n":
+            f.seek(-2, os.SEEK_CUR)
+
+        # Read forward to get the last line
+        line = f.readline()
+
+        # Leave the cursor where it should be for the next call
+        f.seek(-1 * (len(line) + 2), os.SEEK_CUR)
+
+        return line.decode("utf-8").strip()
+
+    def get_last_device_entry(self, device_id) -> list[str]:
         """Get the last line of the metrics file."""
         with self.metrics_file.open("rb") as f:
             # Move to the end of the file
-            f.seek(os.SEEK_END)
+            f.seek(-1, os.SEEK_END)
 
-            # Read backwards until we find a newline
-            while f.read(1) != b"\n":
-                f.seek(-2, os.SEEK_CUR)
+            try:
+                while (line := self.get_previous_line(f).split(",")) and line[1] != str(
+                    device_id
+                ):
+                    continue
+            except OSError:
+                raise DeviceIdError(device_id)
 
-            # Read forward to get the last line
-            return f.readline().decode("utf-8").strip()
+            return line
 
-    def get_metrics(self) -> dict[str, Any]:
+    def get_metrics(self) -> list[dict[str, Any]]:
         """Get XPU metrics."""
-        # Get the first and last lines of xpu_metrics.txt
-        return {self.first_line: self.get_last_line()}
+        results = []
+        try:
+            for device_id in range(4):
+                results.append(
+                    {
+                        k: v
+                        for k, v in zip(
+                            self.headers, self.get_last_device_entry(device_id)
+                        )
+                    }
+                )
+        except DeviceIdError:
+            pass
+        return results
+
+
+class XpuMetricsV2(UsageMetrics):
+    """Note that we expect metrics to be streaming to a file already."""
+
+    @classmethod
+    def are_available(cls) -> bool:
+        """Check whether XPU is available."""
+        return which("xpu-smi") is not None
+
+    def get_metrics(self) -> list[dict[str, str]]:
+        """Get XPU metrics."""
+        completed_process = run(
+            ["xpu-smi", "dump", "--metrics", "0,1", "--device", "-1", "-n", "1"],
+            capture_output=True,
+            check=True,
+            text=True,
+        )
+        output = completed_process.stdout.splitlines()
+        headers = [x.strip() for x in output[0].split(",")]
+        return [
+            {k: v.strip() for k, v in zip(headers, line.split(","))}
+            for line in output[1:]
+        ]
 
 
 class CudaMetrics(UsageMetrics):
@@ -60,7 +123,9 @@ class CudaMetrics(UsageMetrics):
         """Check whether CUDA is available."""
         return torch.cuda.is_available()
 
-    def get_metrics(self) -> dict[str, Any]:
-        return {
-            "gpu_util": torch.cuda.utilization(),
-        }
+    def get_metrics(self) -> list[dict[str, Any]]:
+        return [
+            {
+                "gpu_util": torch.cuda.utilization(),
+            }
+        ]
