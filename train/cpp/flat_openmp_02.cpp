46f450fb173790cedce09d497426725d97748d87
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-07-30 10:49:17 +0100

    css_matrixmul

---
+++ train/cpp/flat_openmp_02.cpp
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+#define SIZE 320
+
+int num_devices = omp_get_num_devices();
+//int chunksize = SIZE/num_devices;
+
+int main(void)
+{
+  int num_devices = omp_get_num_devices();
+  printf ("num_devices = %d\n", num_devices);
+  return 0;
+ // int *A;
+ // A = new int[sizeof(int) * SIZE];
+
+ // printf ("num_devices = %d\n", num_devices);
+
+ // for (int i = 0; i < SIZE; i++)
+ //     A[i] = -9;
+
+ // #pragma omp parallel for
+ // for (int id = 0; id < num_devices; id++) {
+ //     #pragma omp target teams distribute parallel for device(id) \
+ //         map(tofrom: A[id * chunksize : chunksize])
+ //     for (int i = id * chunksize; i < (id + 1) * chunksize; i++) {
+ //         A[i] = i;
+ //     }
+ // }
+
+ // for (int i = 0; i < SIZE; i++)
+ //   if (A[i] != i)
+ //     printf ("Error in: %d\n", A[i]);
+    //else
+      //printf ("%d\n", A[i]);
+
+}
--- train/scripts/my_gemm.py
+++ train/scripts/my_gemm.py
@@ -1,21 +1,23 @@
 """Fine tune Aurora weather model."""
 
-from datetime import datetime
+import sys
 import time
+from datetime import datetime
+
 print("importing...", flush=True)
 import argparse
 import os
 import re
+import traceback
 import warnings
 from pathlib import Path
 
-import warnings
-import traceback
 
 def custom_warn(message, category, filename, lineno, file=None, line=None):
-    
+
     print(datetime.now().replace(microsecond=0), f"\n⚠️ {category.__name__}: {message}")
     traceback.print_stack(limit=5)
+
 
 warnings.showwarning = custom_warn
 
@@ -23,20 +25,20 @@ warnings.filterwarnings(
     "ignore", category=UserWarning, message="TypedStorage is deprecated"
 )
 
+from functools import partial
+
+import intel_extension_for_pytorch as ipex
 import torch
 import torch.nn as nn
 from aurora_loss import mae
 from dataset import AuroraDataset, aurora_collate_fn
+from intel_extension_for_pytorch.xpu.utils import XPUComputeEng
 from torch.distributed import all_gather, destroy_process_group, init_process_group
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp import ShardingStrategy
 from torch.utils.data import DataLoader, DistributedSampler
 
 from aurora import Aurora, AuroraSmall
-from functools import partial
-
-import intel_extension_for_pytorch as ipex
-from intel_extension_for_pytorch.xpu.utils import XPUComputeEng
 
 # unset affinity mask
 os.environ.pop("ZE_AFFINITY_MASK", None)
@@ -45,10 +47,10 @@ os.environ.pop("ZE_AFFINITY_MASK", None)
 M, K, N = 512, 512, 512  # Dimensions of the matrices
 iterations = 100  # How many GEMMs to perform
 
-def main():
-    device_type = "xpu"
 
-    device = f"{device_type}"
+def main():
+    device = f"xpu:{sys.argv[1]}"
+
     print(datetime.now().replace(microsecond=0), f"Using {device=}", flush=True)
 
     A = torch.randn(M, K, device=device)
